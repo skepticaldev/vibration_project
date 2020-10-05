ea9debe5796af9afe1d46265bb856bcd43bc88ca
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2020-10-05 19:58:11 -0300

    some changes

--- sensor_test.py
+++ sensor_test.py
@@ -25,14 +25,14 @@ Device_Address = 0x68
 bus = smbus.SMBus(1)
 
 #--------------------------------------------------------
-#									Measured offsets
+#	Measured offsets
 #--------------------------------------------------------
 AX_OFFSET = 0
 AY_OFFSET = 0
 AZ_OFFSET = 0
 
 #--------------------------------------------------------
-#									Calibration parameters
+#	Calibration parameters
 #--------------------------------------------------------
 #amount used to calculate average
 buffer_size = 1000
@@ -40,7 +40,7 @@ buffer_size = 1000
 accel_error_range = 10
 
 #--------------------------------------------------------
-# 								Config MPU function
+# Config MPU function
 #--------------------------------------------------------
 def MPU_INIT():
 	#write to sample rate register
@@ -87,7 +87,7 @@ def read_raw_data(addr, offset):
 
 
 #--------------------------------------------------------
-#								Calculate samples average
+#	Calculate samples average
 #--------------------------------------------------------
 def AVG_DATA():
 
@@ -134,6 +134,8 @@ def CALIBRATE():
 		AX_OFFSET = c_ax_offset
 		AY_OFFSET = c_ay_offset
 		AZ_OFFSET = c_az_offset
+
+		print(AX_OFFSET, AY_OFFSET, AZ_OFFSET)
 		
 		avg_ax, avg_ay, avg_az = AVG_DATA()
 
@@ -156,7 +158,7 @@ def CALIBRATE():
 			break
 
 #--------------------------------------------------------
-#												Execution
+#	Execution
 #--------------------------------------------------------
 
 MPU_INIT()
