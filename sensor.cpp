76011bb79f169b6b7109a17fe7d22700648026e8
Skeptical <philipe.cavalcanti.lp@gmail.com>
2020-11-23 23:12:45 -0300

    Save

--- sensor.cpp
+++ sensor.cpp
@@ -8,6 +8,7 @@
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
 
+/* MPU6050 Registers and addresses */
 #define PWR_MGMT_1 0x6B
 #define SMPLRT_DIV 0x19
 #define DLPF_CFG 0x26
@@ -23,70 +24,52 @@
 #define GYRO_YOUT_H 0x45
 #define GYRO_ZOUT_H 0x47
 
+// MPU6050 device address
 #define DEVICE_ADDRESS 0x68
 
-void write_byte_data(int file, __u8 reg, __u8 byte)
-{
-	char buf[2];
-	
-	buf[0] = reg;
-	buf[1] = byte;
-	
-	if(write(file, buf, 2) != 2)
-	{
-		printf("Error to write register");
-	}
-}
- 
-void read_raw_data(int file, __u8 reg)
-{
-	int8_t buff[2];
-	
-	char r[1];
-	r[0] = reg;
 
-	write(file, r, 1);
-	read(file,buff,2);
+//--------------------------
+// Function declarations
+//--------------------------
 
-	int value = (buff[0]<<8)+buff[1];
-}
+//MPU config function
+void mpu_init(int file);
 
-void mpu_init(int file)
-{
-	write_byte_data(file, SMPLRT_DIV, 0x00);
-	write_byte_data(file, PWR_MGMT_1, 0x01);
-	write_byte_data(file, CONFIG, 0x00);
-	write_byte_data(file, GYRO_CONFIG, 0x18);
-	write_byte_data(file, ACCEL_CONFIG, 0x18);
-	write_byte_data(file, INT_ENABLE, 0x01);
-	write_byte_data(file, DLPF_CFG, 0x00);
-	write_byte_data(file, FIFO_EN, 0x08);
-}
+// Write config values to registers
+void write_byte_data(int file, __u8 reg, __u8 byte);
+
+// Read MPU raw values LSB/g
+int16_t read_raw_data(int file, __u8 reg);
+
+// Get signed values
+int16_t signed_value(int16_t value);
 
 using namespace std;
 
-int main()
-{
+
+// Main function
+int main() {
 	int file_i2c;
 	int length;
 	unsigned char buffer[60] ={0};
 
-	//------OPEN THE I2C BUS------
+	//------ OPEN THE I2C BUS ------
 	char *filename = (char*)"/dev/i2c-1";
-	if((file_i2c = open(filename, O_RDWR)) < 0)
-	{
+	if((file_i2c = open(filename, O_RDWR)) < 0) {
 		//ERROR HANDLING
 		printf("Failed to open the i2c bus");
 		return 1;
 	}
 
-	if (ioctl(file_i2c, I2C_SLAVE, DEVICE_ADDRESS) < 0)
-	{
+	if (ioctl(file_i2c, I2C_SLAVE, DEVICE_ADDRESS) < 0) {
 		printf("Failed to acquire bus access and/or talk to slave.\n");
 		return 2;
 	}
 
+	// MPU configuration
 	mpu_init(file_i2c);
+
+	
 	auto start = chrono::steady_clock::now();
 	int i = 0;
 	for(i=0;i<=999;i++){
@@ -102,4 +85,70 @@ int main()
 	return 0;
 }
 
+void mpu_init(int file) {
+	// write to sample rate register
+	write_byte_data(file, SMPLRT_DIV, 0x00);
 
+	//write to power management register
+	write_byte_data(file, PWR_MGMT_1, 0x01);
+	
+	//write to configuration register
+	write_byte_data(file, CONFIG, 0x00);
+
+	//write to gyro configuration register
+	write_byte_data(file, GYRO_CONFIG, 0x18);
+
+	//write to accel configuration register
+	write_byte_data(file, ACCEL_CONFIG, 0x18);
+
+	//write to interrupt enable register
+	write_byte_data(file, INT_ENABLE, 0x01);
+	
+	//write to DLPF register
+	write_byte_data(file, DLPF_CFG, 0x00);
+
+	//write to FIFO_EN
+	write_byte_data(file, FIFO_EN, 0x08);
+}
+
+
+void write_byte_data(int file, __u8 reg, __u8 byte){
+	//payload to deliver to register
+	char payload[2];
+	
+	payload[0] = reg;
+	payload[1] = byte;
+	
+	if(write(file, payload, 2) != 2) {
+		printf("Error to write register");
+	}
+}
+
+int16_t read_raw_data(int file, __u8 reg){
+	// buffer to receive high and low values
+	int8_t buffer[2];
+	
+	char payload[1];
+	
+	payload[0] = reg;
+
+	write(file, payload, 1);
+	read(file,buffer,2);
+
+	int16_t value = (buffer[0]<<8)+buffer[1];
+
+	return value;
+}
+
+int16_t signed_value(int16_t value){
+	if(value>32768){
+		value = value - 65536;
+	}
+	return value;
+}
+
+double avg_data(){
+	double avg_ax = 0, avg_ay = 0, avg_az = 0;
+
+	int i = 0;
+}
